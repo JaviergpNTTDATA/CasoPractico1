68f59918b093fd9e38ed899088a5a0eb0b5dd082
javierGP <jgarcpor@emeal.nttdada.com>
2026-03-24 15:21:13 +0100

    Añadimos test unitarios a clientes

---
+++ src/test/java/org/javinttdata/cliente/ClienteServiceTest.java
@@ -0,0 +1,68 @@
+package org.javinttdata.cliente;
+
+import org.javinttdata.cliente.model.Cliente;
+import org.javinttdata.cliente.repository.ClientesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Clase que se encarga de hacer test
+ */
+class ClientesRepositoryTest {
+
+    //Declaramos el repositorio
+    private ClientesRepository repository;
+
+    /**
+     * Antes de cada test inicializamos el repositorio
+     */
+    @BeforeEach
+    void setUp() {
+        repository = new ClientesRepository();
+    }
+
+    /**
+     * En este test comprobamos que se guarde correctamente el cliente
+     */
+    @Test
+    void deberiaGuardarClienteCorrectamente() {
+        Cliente cliente = new Cliente("Juan", "Perez", "12345678A", "juan@test.com", "600000000");
+
+        repository.guardar(cliente);
+
+        assertEquals(1, repository.obtenerTodos().size());
+        assertNotNull(cliente.getId());
+    }
+
+    /**
+     * En este test comprobamos que no pueda añadir dos cliente con el mismo dni, en este caso tiene que lanzar una excepcion para que sea valido el test
+     */
+    @Test
+    void noDebePermitirDniDuplicado() {
+        Cliente c1 = new Cliente("Juan", "Perez", "12345678A", "juan@test.com", "600000000");
+        Cliente c2 = new Cliente("Ana", "Lopez", "12345678A", "ana@test.com", "611111111");
+
+        repository.guardar(c1);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            repository.guardar(c2);
+        });
+    }
+
+    /**
+     * Test que comprueba que funcione correctamente la busqueda de cliente por dni
+     */
+    @Test
+    void deberiaBuscarPorDni() {
+        Cliente cliente = new Cliente("Juan", "Perez", "12345678A", "juan@test.com", "600000000");
+
+        repository.guardar(cliente);
+
+        Cliente encontrado = repository.buscarPorDni("12345678A");
+
+        assertNotNull(encontrado);
+        assertEquals("Juan", encontrado.getNombre());
+    }
+}
---
+++ src/test/java/org/javinttdata/ejemplo.java
@@ -0,0 +1,4 @@
+package org.javinttdata;
+
+public class ejemplo {
+}
